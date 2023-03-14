19ba6867da2a29a747c73d2b2f5a9ae248f2ba6a
angenehms <angenehms@gmail.com>
2023-03-14 21:08:15 +0900

    230314 todolist signin page MUI backgruond complete

---
+++ todolist/src/component/ImgArrayForRandom/ImgArrayForRandom.jsx
@@ -0,0 +1,27 @@
+import SigninPageImg1 from '../../img/SigninPageImg/planner1.jpg';
+import SigninPageImg2 from '../../img/SigninPageImg/planner2.jpg';
+import SigninPageImg3 from '../../img/SigninPageImg/planner3.jpg';
+import SigninPageImg4 from '../../img/SigninPageImg/planner4.jpg';
+import SigninPageImg5 from '../../img/SigninPageImg/planner5.jpg';
+import SigninPageImg6 from '../../img/SigninPageImg/planner6.jpg';
+import SigninPageImg7 from '../../img/SigninPageImg/planner7.jpg';
+import SigninPageImg8 from '../../img/SigninPageImg/planner8.jpg';
+import SigninPageImg9 from '../../img/SigninPageImg/planner9.jpg';
+import SigninPageImg10 from '../../img/SigninPageImg/planner10.jpg';
+import SigninPageImg11 from '../../img/SigninPageImg/planner11.jpg';
+import SigninPageImg12 from '../../img/SigninPageImg/planner12.jpg';
+
+export const ImgArrayForRandom = [
+    SigninPageImg1,
+    SigninPageImg2,
+    SigninPageImg3,
+    SigninPageImg4,
+    SigninPageImg5,
+    SigninPageImg6,
+    SigninPageImg7,
+    SigninPageImg8,
+    SigninPageImg9,
+    SigninPageImg10,
+    SigninPageImg11,
+    SigninPageImg12
+];
--- todolist/src/page/Signin.jsx
+++ todolist/src/page/Signin.jsx
@@ -6,10 +6,16 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ImgArrayForRandom } from '../component/ImgArrayForRandom/ImgArrayForRandom';
 
 const Signin = () => {
 
   const isSignin = !!localStorage.getItem("accessToken");
+
+  const imgRandomNumber = Math.floor(Math.random() * ImgArrayForRandom.length);
+  const randomBackgroundImg = ImgArrayForRandom[imgRandomNumber]
 
   return (
 
@@ -17,20 +23,43 @@ const Signin = () => {
 
       <Navigate to="/todo"/> : // <Todo/> 로 가도록 설정해줄 수도 있지만 그렇게 하면 url 이 /signin 인채로 남음 ! 그래서 Navigate 로 하는 게 사용자경험에 더 좋다
       
-      <Container component="main" maxWidth="xs">
 
-        <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: `url(${randomBackgroundImg})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} elevation={6} square>
 
-          <Avatar sx={{ boxShadow:5, m: 2, bgcolor:"secondary.main" }}>
-            <LockOutlinedIcon/> 
-          </Avatar>
+          <Container component="main" maxWidth="xs">
 
-          <Typography component="h1" variant="h5">Signin Page</Typography>
-          <SigninForm/>  
+            <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
 
-        </Box>
+              <Avatar sx={{ boxShadow:5, m: 2, bgcolor:"secondary.main" }}>
+                <LockOutlinedIcon/> 
+              </Avatar>
 
-      </Container> 
+              <Typography component="h1" variant="h5">Signin Page</Typography>
+              <SigninForm/>  
+
+            </Box>
+
+          </Container> 
+
+        </Grid>
+        
+      </Grid>
   
   )
 }
