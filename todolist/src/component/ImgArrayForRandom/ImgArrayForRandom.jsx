7c7eadf0deddcdc81bbcfe28a4e50bbdb05a862a
angenehms <angenehms@gmail.com>
2023-03-14 21:18:46 +0900

    230314 todolist signinPageImg rename

--- todolist/src/component/ImgArrayForRandom/ImgArrayForRandom.jsx
+++ todolist/src/component/ImgArrayForRandom/ImgArrayForRandom.jsx
@@ -1,15 +1,15 @@
-import SigninPageImg01 from '../../img/SigninPageImg/planner01.jpg';
-import SigninPageImg02 from '../../img/SigninPageImg/planner02.jpg';
-import SigninPageImg03 from '../../img/SigninPageImg/planner03.jpg';
-import SigninPageImg04 from '../../img/SigninPageImg/planner04.jpg';
-import SigninPageImg05 from '../../img/SigninPageImg/planner05.jpg';
-import SigninPageImg06 from '../../img/SigninPageImg/planner06.jpg';
-import SigninPageImg07 from '../../img/SigninPageImg/planner07.jpg';
-import SigninPageImg08 from '../../img/SigninPageImg/planner08.jpg';
-import SigninPageImg09 from '../../img/SigninPageImg/planner09.jpg';
-import SigninPageImg10 from '../../img/SigninPageImg/planner10.jpg';
-import SigninPageImg11 from '../../img/SigninPageImg/planner11.jpg';
-import SigninPageImg12 from '../../img/SigninPageImg/planner12.jpg';
+import SigninPageImg01 from '../../img/SigninPageImg/SigninPageImg01.jpg';
+import SigninPageImg02 from '../../img/SigninPageImg/SigninPageImg02.jpg';
+import SigninPageImg03 from '../../img/SigninPageImg/SigninPageImg03.jpg';
+import SigninPageImg04 from '../../img/SigninPageImg/SigninPageImg04.jpg';
+import SigninPageImg05 from '../../img/SigninPageImg/SigninPageImg05.jpg';
+import SigninPageImg06 from '../../img/SigninPageImg/SigninPageImg06.jpg';
+import SigninPageImg07 from '../../img/SigninPageImg/SigninPageImg07.jpg';
+import SigninPageImg08 from '../../img/SigninPageImg/SigninPageImg08.jpg';
+import SigninPageImg09 from '../../img/SigninPageImg/SigninPageImg09.jpg';
+import SigninPageImg10 from '../../img/SigninPageImg/SigninPageImg10.jpg';
+import SigninPageImg11 from '../../img/SigninPageImg/SigninPageImg11.jpg';
+import SigninPageImg12 from '../../img/SigninPageImg/SigninPageImg12.jpg';
 
 export const ImgArrayForRandom = [
     SigninPageImg01,
