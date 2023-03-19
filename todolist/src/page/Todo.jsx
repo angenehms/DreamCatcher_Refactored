533e77a2f398b72beebf39c6f563cbb77ee2d1da
angenehms <angenehms@gmail.com>
2023-03-19 21:42:18 +0900

    230319 todolist layout correction

--- todolist/src/page/Todo.jsx
+++ todolist/src/page/Todo.jsx
@@ -52,7 +52,7 @@ const Todo = () => {
         <Box sx={{width:"90%"}}>
           <TodoCreateForm readTodoFunction={readTodoFunction}/>
         </Box>
-        <Box style={{height:"50vh", width:"100%", marginRight:"5%", marginTop:"7%"}}>
+        <Box style={{height:"50vh", width:"100%", marginTop:"7%"}}>
           <TodoUpdateDeleteForm readTodoFunction={readTodoFunction} todoListInfo={todoListInfo}/>
         </Box>
         <SignoutForm/>
