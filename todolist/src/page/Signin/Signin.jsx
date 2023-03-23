36054d85a1155f6884f55413080d3a9ac282870c
angenehms <angenehms@gmail.com>
2023-03-23 18:13:24 +0900

    230323 todolist Signin Page styled component compelete

--- todolist/src/page/Signin/Signin.jsx
+++ todolist/src/page/Signin/Signin.jsx
@@ -4,18 +4,14 @@ import { Navigate } from 'react-router-dom'
 import Typography from '@mui/material/Typography';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Avatar from '@mui/material/Avatar';
-import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import CssBaseline from '@mui/material/CssBaseline';
-import { ImgArrayForRandom } from '../../component/ImgArrayForRandom/ImgArrayForRandom';
+import * as S from './SigninStyle'
 
 const Signin = () => {
 
   const isSignin = !!localStorage.getItem("accessToken");
-
-  const imgRandomNumber = Math.floor(Math.random() * ImgArrayForRandom.length);
-  const randomBackgroundImg = ImgArrayForRandom[imgRandomNumber]
 
   return (
 
@@ -23,45 +19,51 @@ const Signin = () => {
 
       <Navigate to="/todo"/> : // <Todo/> 로 가도록 설정해줄 수도 있지만 그렇게 하면 url 이 /signin 인채로 남음 ! 그래서 Navigate 로 하는 게 사용자경험에 더 좋다
 
-      <Grid container component="main" sx={{ height: '100vh' }}>
+      <S.WrapperMuiGrid container component="main">
 
         <CssBaseline />
         
-        <Grid
+        <S.RandomImgMuiGrid
+          className="randomImgMuiGrid"
           item
           xs={false}
           sm={4}
           md={7}
           sx={{
-            backgroundImage: `url(${randomBackgroundImg})`,
-            backgroundRepeat: 'no-repeat',
+            
             backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900]
+
           }}
         />
 
-        <Grid item xs={12} sm={8} md={5} elevation={6} square>
+        <Grid 
+          item 
+          xs={12} 
+          sm={8} 
+          md={5} 
+          elevation={6} 
+          square
+        >
 
           <Container component="main" maxWidth="xs">
 
-            <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <S.SigninFormBox>
 
-              <Avatar sx={{ boxShadow:5, m: 2, bgcolor:"secondary.main" }}>
+              <Avatar sx={{ boxShadow: 5, margin: 2, bgcolor: "secondary.main" }}>
                 <LockOutlinedIcon/> 
               </Avatar>
 
               <Typography component="h1" variant="h5">Signin Page</Typography>
               <SigninForm/>  
 
-            </Box>
+            </S.SigninFormBox>
 
           </Container> 
 
         </Grid>
         
-      </Grid>
+      </S.WrapperMuiGrid>
   
   )
 }
---
+++ todolist/src/page/Signin/SigninStyle.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import { ImgArrayForRandom } from '../../component/ImgArrayForRandom/ImgArrayForRandom';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+
+const imgRandomNumber = Math.floor(Math.random() * ImgArrayForRandom.length);
+const randomBackgroundImg = ImgArrayForRandom[imgRandomNumber]
+
+export const WrapperMuiGrid = styled(Grid)`
+    height: 100vh;
+`
+
+export const RandomImgMuiGrid = styled(Grid)`
+    &.randomImgMuiGrid {
+    background-image: url(${randomBackgroundImg});
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+}
+`
+
+export const SigninFormBox = styled(Box)`
+    margin-top: 30%;
+    display: flex;
+    flex-direction: column; 
+    align-items: center;
+`
