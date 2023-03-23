57427ad4e791cf63380afa5cd7e663f7513475fa
angenehms <angenehms@gmail.com>
2023-03-23 15:47:50 +0900

    230323 todolist Signup Page component styled without Avatar Mui component which need to retry

--- todolist/src/page/Signup/Signup.jsx
+++ todolist/src/page/Signup/Signup.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import SignupForm from '../../component/SignupForm/SignupForm';
 import { Navigate } from 'react-router-dom'
-import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import CssBaseline from '@mui/material/CssBaseline';
+import * as S from './SignupStyle'
 
 const Signup = () => {
 
@@ -17,18 +17,17 @@ const Signup = () => {
     isSignin ? 
 
       <Navigate to="/todo"/> : // <Todo/> 로 가도록 설정해줄 수도 있지만 그렇게 하면 url 이 /signup 인채로 남음 ! 그래서 Navigate 로 하는 게 사용자경험에 더 좋다
-      <Container component="main" maxWidth="xs">
+      <S.WrapperMuiContainer component="main" maxWidth="xs">
         <CssBaseline/>
-        <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        
-          <Avatar sx={{ boxShadow:5, m: 2, bgcolor:"secondary.main" }}>
+        <Box>
+          <Avatar sx={{ boxShadow: 5, margin: 2, bgcolor: "secondary.main" }}> 
+          {/* Avatar 컴포넌트의 스타일도 스타일드 컴포넌트로 빼고 싶은데 잘 안되었다 .. ! */}
             <LockOutlinedIcon/> 
           </Avatar>
-          
           <Typography component="h1" variant="h5">Signup Page</Typography>
           <SignupForm/>
         </Box>
-      </Container>
+      </S.WrapperMuiContainer>
       
   )
 }
---
+++ todolist/src/page/Signup/SignupStyle.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import Container from '@mui/material/Container';
+
+export const WrapperMuiContainer = styled(Container)`
+    margin-top : 100px;
+
+    .MuiBox-root {
+        display : flex; 
+        flex-direction : column;
+        align-items: center;
+    }
+`
