638d0ff375b7776ef1f9a1257ca9c4af0f9cd128
angenehms <angenehms@gmail.com>
2023-03-24 19:12:03 +0900

    230324 todolist SignupForm component custom hook complete

--- todolist/src/component/SignupForm/SignupForm.jsx
+++ todolist/src/component/SignupForm/SignupForm.jsx
@@ -1,30 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { sendForSignupFunction } from '../../api/Auth';
 import { TextField, Button, Grid, Link } from '@mui/material';
+import { useInput } from '../../hook/useInput';
 
 // sendForSignupFunction 는 회원가입을 위해 필요내용을 보내주는 틀(함수)
 
 const SignupForm = () => {
 
     const navigate = useNavigate();
-    const [identification, setIdentification] = useState("");
-    const [password, setPassword] = useState("");
-    const [passwordChecking, setPasswordChecking] = useState("");
+    const [identification, identificationChangeFunction, identificationValidity] = useInput("");
+    const [password, passwordChangeFunction, passwordValidity] = useInput(""); 
+    const [passwordCheck, passwordCheckChangeFunction] = useInput("");
+    // 배열의 두번째 인덱스인 함수들은 사실 똑같은 역할을 하지만 이름만 다르다고 생각한다! 그럼 왜 굳이 이름을 다르게 하는걸까? 왜냐면 의미는 같지만 두번 선언할 수 없게 되어있다. 이름을 갖게하면 에러가 뜬다.
 
-    const changeIdentificationValue = (e) => {
-        setIdentification(e.target.value);
-    }; // 아이디 인풋값 변경함수
-    const changePasswordValue = (e) => {
-        setPassword(e.target.value);
-    }; // 비밀번호 인풋값 변경함수
-    const changePasswordChecking = (e) => {
-        setPasswordChecking(e.target.value);
-    }; // 비밀번호확인 인풋값 변경함수
+    const passwordCheckValidity = () => {
+        if ( password.inputValue === passwordCheck.inputValue ) {
+            return true;
+        } else {
+            return false;
+        }
+    };
 
     const sendContentsForSignup = {
-        email : `${identification}`,
-        password : `${password}` 
+        email : `${identification.inputValue}`,
+        password : `${password.inputValue}` 
     };
 
     const signupFunction = async (e) => { // 회원가입 시켜주는 함수 그후 signin 페이지 이동
@@ -35,54 +35,49 @@ const SignupForm = () => {
         }
 
         // console.log(res);
-;    };
-
-    const isEqualPassword = (password === passwordChecking); // 비밀번호 확인 일치여부
-    const validityCheckFunction = () => { // 유효성검사 함수
-        if (identification.includes("@") && password.length >= 8 && isEqualPassword) {
-            return true;
-        } else {
-            return false;
-        } 
     };
 
-    const validityResult = validityCheckFunction(); // 유효성검사 결과값(불리언)
+    const validityResult = identificationValidity && passwordValidity && passwordCheckValidity(); 
+    // 유효성검사 결과값(불리언)
 
   return (
 
     <form>
 
         <TextField 
+            id="Identification"
             label="Email Address" 
             type="text" 
             sx={{ mt: 5 }} 
             required 
             fullWidth 
             name="email" 
-            value={identification} 
-            onChange={changeIdentificationValue}
+            value={identification.inputValue} 
+            onChange={identificationChangeFunction}
         />
 
         <TextField 
+            id="Password"
             label="Password" 
             type="password" 
             sx={{ mt: 1.5 }} 
             required 
             fullWidth 
             name="password" 
-            value={password} 
-            onChange={changePasswordValue}
+            value={password.inputValue} 
+            onChange={passwordChangeFunction}
         />
 
         <TextField 
+            id="PasswordCheck"
             label="Password Check" 
             type="password" 
             sx={{ mt: 1.5 }} 
             required 
             fullWidth 
             name="passwordCheck" 
-            value={passwordChecking} 
-            onChange={changePasswordChecking}
+            value={passwordCheck.inputValue} 
+            onChange={passwordCheckChangeFunction}
         />
 
         <Button 
@@ -90,7 +85,7 @@ const SignupForm = () => {
             type="submit" 
             sx={{ mt: 3, mb: 2 }} 
             fullWidth 
-            disabled={!validityResult} 
+            disabled={!validityResult}
             onClick={signupFunction}
         >
             Signup
@@ -102,6 +97,7 @@ const SignupForm = () => {
                 <Link href="/signin">Go To Signin</Link>
             </Grid>
         </Grid>
+
     </form>
 
   )
--- todolist/src/hook/useInput.jsx
+++ todolist/src/hook/useInput.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
 
-const useInput = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+export function useInput () {
+
+    const [inputValue, setInputValue] = useState("");
+    const [validity, setValidity] = useState(false);
+    
+    const validityCheckFunction = (e) => {
+
+        setInputValue(e.target.value);
+
+        if (e.target.id === "Identification" && e.target.value.includes("@")) {
+            setValidity(true);
+        } else if (e.target.id === "Password" && e.target.value.length >= 8) {
+            setValidity(true);
+        } else {
+            setValidity(false);
+        }
+    }
+    
+  return [{inputValue, setInputValue}, validityCheckFunction, validity] 
+
 }
 
-export default useInput
